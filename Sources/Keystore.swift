32638c13d6214598bb4e0088f2253c489619d5fc
Alejandro Isaza <al@isaza.ca>
2017-12-13 22:28:33 -0800

    Add Keystore definition

--- Sources/Keystore.swift
+++ Sources/Keystore.swift
@@ -5,3 +5,61 @@
 // file LICENSE at the root of the source code distribution tree.
 
 import Foundation
+
+/// Key derivation function parameters
+public struct KDFParams: Codable {
+    /// Random salt.
+    public var salt: String
+
+    /// Desired key length in bytes.
+    public var dklen: Int
+
+    /// CPU/Memory cost factor.
+    public var n: Int?
+
+    /// Parallelization factor (1..232-1 * hLen/MFlen).
+    public var p: Int?
+
+    /// Block size factor.
+    public var r: Int?
+}
+
+public struct CipherParams: Codable {
+    public var iv: String
+}
+
+/// Encrypted private key and crypto parameters.
+public struct Crypto: Codable {
+    /// Encrypted data.
+    public var cipherText: String
+
+    /// Cipher algorithm.
+    public var cipher: String
+
+    /// Cipher parameters.
+    public var cipherParams: CipherParams
+
+    /// Key derivation function, must be scrypt.
+    public var kdf: String
+
+    /// Key derivation function parameters.
+    public var kdfparams: KDFParams
+
+    /// Message authentication code.
+    public var mac: String
+}
+
+/// Keystore wallet definition.
+public struct Keystore: Codable {
+    /// Ethereum address, optional.
+    public var address: String?
+
+    /// Wallet UUID, optional.
+    public var id: String?
+
+    /// Encryped private key and crypto parameters.
+    public var crypto: Crypto
+
+    /// Keystore version, must be 3.
+    public var version: Int
+}
